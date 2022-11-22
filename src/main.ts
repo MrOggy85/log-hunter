524a85db4df4059707223700321497cbdc8d6ea2
Oskar Okuno <2677573+MrOggy85@users.noreply.github.com>
2022-11-22 21:21:27 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 const p = Deno.run({
-  cmd: ["tail", "-n", "100", "./fixtures/read.log"],
+  cmd: ["tail", "-n", "500", "./fixtures/read.log"],
   stdout: "piped",
 });
 
@@ -8,53 +8,139 @@ const { code } = await p.status();
 const rawOutput = await p.output();
 
 const rawText = new TextDecoder().decode(rawOutput);
+
+/**
+ * Output of command:
+ * `iptables -v -n -L DOCKER-USER`
+ */
+async function getBannedIps() {
+  const p = Deno.run({
+    cmd: ["cat", "./fixtures/iptables-output.txt"],
+    stdout: "piped",
+  });
+
+  // const { code } = await p.status();
+  await p.status();
+
+  const o = await p.output();
+
+  const iptablesRawText = new TextDecoder().decode(o);
+  iptablesRawText.split("\n").forEach((x) => {
+    console.log("hej", x.split("\\t"));
+  });
+}
 // console.log("hej", hej);
+
+// const FORBIDDEN_PATHS = ["admin.php"];
+
+type SEVERITY =
+  | 0
+  | 1
+  | 2;
+
+type Violation = {
+  path: string;
+  severity: SEVERITY;
+};
+
+const HIGH_SEVERITY_PATHS = [
+  "/admin.php",
+  "/wp-content",
+  "/wp-commentin.php",
+  "/wp-login.php",
+  "/class-wp-widget-archives.php",
+  "/debug",
+  "/.env",
+  "/.DS_Store",
+  "/ecp/Current/exporttool/microsoft.exchange",
+  "/.git",
+  "/META-INF",
+  "/config.json",
+];
+
+const violations: Violation[] = [
+  ...HIGH_SEVERITY_PATHS.map((x) => ({
+    path: x,
+    severity: 2 as const,
+  })),
+];
+
+type Ban = {
+  ipAddress: string;
+  banDate: string;
+  logDate: string;
+  reason: string;
+  severity: SEVERITY;
+};
+
+const bannedIps: Ban[] = [];
 
 const allRows: string[] = [];
 
 rawText.split("\n").forEach((row, i) => {
-  // console.log(i, x);
   if (!allRows.includes(row)) {
     allRows.push(row);
   }
 });
 
 function splitIntoParts(row: string) {
-  if (!row) {
-    return;
-  }
   const parts1 = row.split(" - -");
   const ipAddress = parts1[0];
-  // console.log("ipAddress", ipAddress);
   const parts2 = parts1[1];
 
   const dateStartIndex = parts2.indexOf("[") + 1;
   const dateEndIndex = parts2.indexOf("]");
   const date = parts2.substring(dateStartIndex, dateEndIndex);
-  // console.log("date", date);
 
   const part3 = parts2.substring(dateEndIndex + 2);
-  // console.log("part3", part3);
   const requestStartIndex = part3.indexOf('"') + 1;
   const requestEndIndex = part3.lastIndexOf('"');
   const request = part3.substring(requestStartIndex, requestEndIndex);
-  // console.log("request", request);
   const requestParts = request.split(" ");
   const httpVerb = requestParts[0];
   const path = requestParts[1];
   const httpProtocol = requestParts[2];
-  console.log({
+
+  const part4 = part3.substring(requestEndIndex + 2).split(" ");
+  const httpStatusCode = Number(part4[0]);
+  const contentLengthInBytes = Number(part4[1]);
+
+  return {
     ipAddress,
     date,
     httpVerb,
     path,
     httpProtocol,
-  });
+    httpStatusCode,
+    contentLengthInBytes,
+  };
 }
 
 allRows.forEach((row) => {
-  splitIntoParts(row);
+  if (!row) {
+    return;
+  }
+  const { path, ipAddress, date } = splitIntoParts(row);
+  // if (path.indexOf("admin.php") !== -1) {
+  //   console.log("path", path);
+  // }
+  const isInViolation = violations.find((x) => path.indexOf(x.path) !== -1);
+
+  if (isInViolation) {
+    // console.log("BAD IP", ipAddress, "because of", path);
+    bannedIps.push({
+      ipAddress,
+      banDate: new Date().toLocaleString(),
+      logDate: date,
+      reason: `path: ${path}`,
+      severity: isInViolation.severity,
+    });
+  }
 });
+
+// console.log("bannedIps", bannedIps);
+
+await getBannedIps();
 
 // console.log("allRows", allRows);
 
